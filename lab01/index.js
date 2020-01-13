de66068aefc833722eface259623f16fea9d36ca
Samantha Billings <shammymeatsix@gmail.com>
2020-01-13 18:17:50 -0500

    commit to master

---
+++ lab01/index.js
@@ -0,0 +1,4 @@
+const Person = require("./person.js");
+
+let p = new Person("Samantha", "Purple");
+console.log(p.name);
---
+++ lab01/person.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(name, color){
+        // this.[argu.] referce to class person
+        this.name = name; 
+        this.color = color; 
+
+    }
+
+    //adding new functions to a class
+
+    speak(){
+        console.log("Hi!");
+    }
+}
+// makes it global to other files: module.exports
+module.exports = Person;
