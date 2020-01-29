37a83acdaecca97f3777df61362df75497f81d15
S-Billings <shammymeatsix@gmail.com>
2020-01-29 17:49:51 -0500

    Finishing changes

--- lab3/public/index.js
+++ lab3/public/index.js
@@ -2,9 +2,15 @@
 const Express = require("express");
 const App = Express();
 const port = 4200;
+// add the html file
+const index = require("./index.html");
 
 App.get("/", function(Request,Response){
     Response.send("hello world");
+});
+
+App.get("/index.html", function(Request, Response){
+    Response.send(index);
 });
 
 App.listen(port, function(){
