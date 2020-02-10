756afb625a68c366d2019b588e0445784cad2835
Samantha Billings <shammymeatsix@gmail.com>
2020-02-10 18:30:13 -0500

    stufff

--- example/fetchExample/index.js
+++ example/fetchExample/index.js
@@ -5,5 +5,9 @@ const port = 80;
 App.use("/", Express.static("public"));
 
 App.get("/api", (req,res)=>{
-    res.json({"error"});
-})
+    res.json({"error: Error Message"});
+}); 
+
+App.listen(port()=>{
+    console.log("server is up up up");
+});
