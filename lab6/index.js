119f43589d469131b7a81be066c0266f6639fcaa
S-Billings <shammymeatsix@gmail.com>
2020-02-19 15:57:27 -0500

    Beginning lab 6

---
+++ lab6/index.js
@@ -0,0 +1,53 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const chalk = require("chalk"); 
+const pokemon = require("json-pokemon");
+
+App.use(Express.json());
+
+App.use("/", Express.static("client/build"));
+
+App.get("/id/:id", (req,res)=>{
+
+    let result = {"error": "Invalid"};
+    let pokemonID = req.params.id;
+
+    pokemon.forEach((value) =>{
+        if (value.id == pokemonID){
+            result = value;
+        }
+    });
+
+    if(result.error){
+        console.log(chalk.red(req.path));
+    } else{
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.get("/name/:name", (req,res)=>{
+    let result = {"error": "Invalid"};
+    let pokemonNames = req.params.name;
+
+    pokemon.forEach((value) =>{
+
+        let pokemonName = pokemonNames;
+        if (value.name == pokemonNames){
+            result = value;
+        }
+    });
+
+    if(result.error){
+        console.log(chalk.red(req.path));
+    } else{
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.listen(port,()=>{
+    console.log("Server is up, up, up.");
+});
