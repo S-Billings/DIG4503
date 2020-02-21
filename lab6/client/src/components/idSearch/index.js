f079b446f82f441faeb34e92acdf292cf855f693
S-Billings <shammymeatsix@gmail.com>
2020-02-21 13:19:27 -0500

    idSearch processing

---
+++ lab6/client/src/components/idSearch/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class idSearch extends React.Component{
+    idClick(event){
+        event.preventDefault();
+
+        let pokeID = document.querySelector("#pokeId");
+
+        fetch("http://localhost:80/id/"+ pokeID.value).then((res)=>{
+            return res.json();
+        }).then((processed)=>{
+            let result =document.querySelector(#response);
+            if(processed.error){
+                result.innerHTML = processed.error;
+            } else{
+                result.innerHTML ="You input is" + processed.name + ".";
+
+            }
+        });
+    };
+    render(){
+        return(
+            <div>
+            <form onSubmit={this.idClick}>
+            <h1>Enter an ID Number</h1>
+            <input type = "text" id = "pokeId"/>
+            <button>Submit</button>
+            </form>
+            </div>
+        );
+    };
+}
+
+export default idSearch;
