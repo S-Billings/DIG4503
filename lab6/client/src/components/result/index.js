55b0d02a67ec4fe82ddb177faafc31e5303470a2
S-Billings <shammymeatsix@gmail.com>
2020-02-21 13:36:02 -0500

    result processing

---
+++ lab6/client/src/components/result/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class result extends React.Component{
+    render(){
+        return(
+            <div>
+
+            <div id = "result"></div>
+
+            </div>
+        );
+    }
+}
+export default result;
