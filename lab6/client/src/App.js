c7c3c55704d83f46697cd59c1b68393669de30aa
S-Billings <shammymeatsix@gmail.com>
2020-02-21 14:08:00 -0500

    final steps

--- lab6/client/src/App.js
+++ lab6/client/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import IdSearch from './components/IdSearch/';
+import NameSearch from './components/NameSearch/';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IdSearch></IdSearch>
+      <NameSearch></NameSearch>
+      <h2>Reporting</h2>
+      <div id="result"></div>
     </div>
+
   );
 }
-
-export default App;
+}
+export default App; 
