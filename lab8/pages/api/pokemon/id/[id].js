c2dcc407cfb38262b55a1caf5753a0af7fabec22
S-Billings <shammymeatsix@gmail.com>
2020-03-19 18:14:39 -0400

    Id fetch is not workiing

--- lab8/pages/api/pokemon/id/[id].js
+++ lab8/pages/api/pokemon/id/[id].js
@@ -1,14 +1,17 @@
 import getPokemon from 'json-pokemon/getPokemon';
 
 export default (req, res) => {
-    let result = {"error": "ID not found"};
+
+    let result = {"error": "Could not find id."};
 
     const id = parseInt(req.query.id);
 
     let pokemon = getPokemon.getPokemonById(id);
 
-    if (pokemon !== null){
+    if(pokemon !== null) {
         result = pokemon;
     }
+
     res.json(result);
+
 }
--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,5 +1,6 @@
-class NameSearch extends React.Component{
+class App extends React.Component{
 
+  
     getName(){
 
         let name = document.querySelector("#pokemonName");
@@ -16,15 +17,40 @@ class NameSearch extends React.Component{
         });
     }
 
+    getId(){
+        let pokeId = document.querySelector("#pokemonId");
+
+        fetch("/api/pokemon/id/" + id.value).then((res)=>{return res.json();}).then((processed)=> {
+            
+            let resultElement = document.querySelector("#results");
+
+            if(processed.error){
+                resultElement.innerHTML ="Could not find your search. Please try again.";
+            } else{
+                resultElement.innerHTML = "The Pokemon ID you entered is: "+ processed.id+" and the ID belongs to: "+processed.name+".";
+
+            }
+        });
+    }
+
     render(){
         return(
+            
             <div>
+                <h3>Search by Name:</h3>
                 <input type="text" id="pokemonName"/>
                 <button onClick={() => {this.getName()}}>Search</button>
+                <div>
+                    <br/>
+                    <h3>Search by ID</h3>
+                <input type="text" id="pokemonId"/>
+                    <button onClick = {() => {this.getId()}}>Search</button>
+                    </div>
+                    <br/>
                 <div id="results"></div>
             </div>
         )
     }
 }
 
-export default NameSearch;
+export default App;
