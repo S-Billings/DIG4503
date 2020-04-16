5f5c3b7ae41a5f2483e121e90d4561ee75f15b6e
S-Billings <shammymeatsix@gmail.com>
2020-04-16 19:23:16 -0400

    Continuing project

--- projectfinal/components/HomePage/index.js
+++ projectfinal/components/HomePage/index.js
@@ -9,8 +9,31 @@ class HomePage extends React.Component{
                 </div>
                 <div>
                     <h4>Our Native Fruit</h4>
-                    <img></img>
+                    <img src ="../img/nativeFroot.png" alt="Animal crossing peach"/>
                     <p>Peaches</p>
+                    <div>
+                        <h4>Meet Our Current Residents!</h4>
+                        <div>
+                            <ol>
+                                <img src="/img/Fuchsia.png" alt="fuchsia a animal crossing villager"/>
+                                <li>Fuchsia</li>
+                                <img src ="img/Sheldon.png" alt="sheldon a animal crossing villager"/>
+                                <li>Sheldon</li>
+                                <img src="img/Erik.png" alt="erik a animal crossing villager"/>
+                                <li>Erik</li>
+                                <img src="img/Bertha.png" alt="bertha a animal crossing villager"/>
+                                <li>Bertha</li>
+                                <img src="img/uglyBitch.png" alt="henry a animal crossing villager"/>
+                                <li>Henry</li>
+                                <img src="img/Bitch.png" alt= "flora a animal crossing villager"/>
+                                <li>Flora</li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Thinking about joining the Indiga island community?</h5>
+                        <button>Click here to find out our next flights.</button>
+                    </div>
                 </div>
             </body>
         )
