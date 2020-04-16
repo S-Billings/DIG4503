172f41a8a6e137f894f1294e757721afc11a3ae2
S-Billings <shammymeatsix@gmail.com>
2020-04-16 11:55:45 -0400

    Starting the final project

---
+++ projectfinal/components/HomePage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            <body>
+                <div>
+                    <h1>Welcome to Indiga's Residetnal Representative's Website</h1>
+                </div>
+                <div>
+                    <h4>Our Native Fruit</h4>
+                    <img></img>
+                    <p>Peaches</p>
+                </div>
+            </body>
+        )
+    }
+}
