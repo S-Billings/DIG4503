063ac1f93556f6198b9f8a28f9f175696df36082
S-Billings <shammymeatsix@gmail.com>
2020-04-18 15:52:09 -0400

    adding a component

--- projectfinal/src/App.js
+++ projectfinal/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from "./components/Homepage/index.js";
+import Application from "./components/Application/index.js";
 
 function App() {
   return (
     <div>
       <Homepage/>
+      <Application/>
     </div>
   );
 }
--- projectfinal/src/components/Application/index.js
+++ projectfinal/src/components/Application/index.js
@@ -9,9 +9,11 @@ class Application extends React.Component{
                     <form>
                         <input type = "text">Enter your name:</input>
                         <input type= "text">Why do you want to live on Indiga?</input>
+                        <input type = "text">What is your favorite form of making bells?</input>
                     </form>
                 </div>
             </body>
         )
     }
 }
+export default Application;
